3a44f6299155d9e0c4420629428ed4f4c08a1c66
Sam Odom <samodom@me.com>
2014-12-24 01:03:54 -0600

    Initial Commit

---
+++ TestableCoreLocation/TestableCoreLocation.h
@@ -0,0 +1,19 @@
+//
+//  TestableCoreLocation.h
+//  TestableCoreLocation
+//
+//  Created by Sam Odom on 12/24/14.
+//  Copyright (c) 2014 Swagger Soft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestableCoreLocation.
+FOUNDATION_EXPORT double TestableCoreLocationVersionNumber;
+
+//! Project version string for TestableCoreLocation.
+FOUNDATION_EXPORT const unsigned char TestableCoreLocationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestableCoreLocation/PublicHeader.h>
+
+
