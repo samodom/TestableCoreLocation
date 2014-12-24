dfe46530c320fe0d1b9c3203daff9809e97c2b72
Sam Odom <samodom@me.com>
2014-12-24 11:46:50 -0600

    Added first shim: CLLocationManager spies

---
+++ README.md
@@ -0,0 +1,7 @@
+TestableCoreLocation
+====================
+
+This is a framework that uses the same class names as some classes from CoreLocation and extends them using the fully qualified name of the real class. This allows for a shim layer between your application code and CoreLocation **only while testing**.
+
+- `CLLocationManager`
+  - [Capturing instance calls](CLLocationManager.md)
---
+++ TestableCoreLocationTests/Tests/ShimMethodForwardingListTests.swift
@@ -0,0 +1,44 @@
+//
+//  ShimMethodForwardingListTests.swift
+//  TestableCoreLocation
+//
+//  Created by Sam Odom on 12/21/14.
+//  Copyright (c) 2014 Swagger Soft. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class ShimMethodForwardingListTests: XCTestCase {
+
+    var forwardingList: ShimMethodForwardingList!
+
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    func testForwardingListWithDefaultForwarding() {
+        forwardingList = ShimMethodForwardingList(shouldForwardByDefault: true)
+        XCTAssertTrue(forwardingList.shouldForwardByDefault, "This forwarding list should forward shim methods by default")
+        XCTAssertTrue(forwardingList.shouldForwardMethodCallWithSelector("someSelector"), "All methods should be forwarded by default")
+        forwardingList.setShouldForwardMethodCallWithSelector("someSelector", false)
+        XCTAssertFalse(forwardingList.shouldForwardMethodCallWithSelector("someSelector"), "The selector should not be forwarded now")
+        forwardingList.setShouldForwardMethodCallWithSelector("someSelector", true)
+        XCTAssertTrue(forwardingList.shouldForwardMethodCallWithSelector("someSelector"), "The selector should now be forwarded again")
+    }
+
+    func testForwardingListWithDefaultNonForwarding() {
+        forwardingList = ShimMethodForwardingList(shouldForwardByDefault: false)
+        XCTAssertFalse(forwardingList.shouldForwardByDefault, "This forwarding list should not forward shim methods by default")
+        XCTAssertFalse(forwardingList.shouldForwardMethodCallWithSelector("someSelector"), "All methods should not be forwarded by default")
+        forwardingList.setShouldForwardMethodCallWithSelector("someSelector", true)
+        XCTAssertTrue(forwardingList.shouldForwardMethodCallWithSelector("someSelector"), "The selector should now be forwarded now")
+        forwardingList.setShouldForwardMethodCallWithSelector("someSelector", false)
+        XCTAssertFalse(forwardingList.shouldForwardMethodCallWithSelector("someSelector"), "The selector should not be forwarded again")
+    }
+
+}
