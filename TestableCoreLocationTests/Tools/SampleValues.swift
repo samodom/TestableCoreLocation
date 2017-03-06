c9fd83c3d2926af95d299ae97e007ee921950d27
Sam Odom <samodom@icloud.com>
2017-03-06 15:43:54 -0700

    Added sample values for testing

---
+++ TestableCoreLocationTests/Tools/SampleValues.swift
@@ -0,0 +1,11 @@
+//
+//  SampleValues.swift
+//  TestableCoreLocation
+//
+//  Created by Sam Odom on 3/6/17.
+//  Copyright © 2017 Swagger Soft. All rights reserved.
+//
+
+import CoreLocation
+
+let SampleCoordinate = CLLocationCoordinate2D(latitude: 12.3456, longitude: 65.4321)
