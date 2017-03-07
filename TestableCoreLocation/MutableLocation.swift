5abae81f50c655197debe0708ae3f2f902a2e906
Sam Odom <samodom@icloud.com>
2017-03-07 16:27:04 -0700

    Added encoding and initializers to MutableLocation

--- TestableCoreLocation/MutableLocation.swift
+++ TestableCoreLocation/MutableLocation.swift
@@ -105,4 +105,159 @@ open class MutableLocation: CLLocation {
         }
     }
 
+
+    /// Override of simple coordinate initializer.
+    public override init(latitude: CLLocationDegrees, longitude: CLLocationDegrees) {
+        super.init(latitude: latitude, longitude: longitude)
+    }
+
+
+    /// Override of stationary location initializer.
+    public override init(
+        coordinate: CLLocationCoordinate2D,
+        altitude: CLLocationDistance,
+        horizontalAccuracy: CLLocationAccuracy,
+        verticalAccuracy: CLLocationAccuracy,
+        timestamp: Date
+        ) {
+
+        super.init(
+            coordinate: coordinate,
+            altitude: altitude,
+            horizontalAccuracy: horizontalAccuracy,
+            verticalAccuracy: verticalAccuracy,
+            timestamp: timestamp
+        )
+    }
+
+
+    /// Override of mobile location initializer.
+    public override init(
+        coordinate: CLLocationCoordinate2D,
+        altitude: CLLocationDistance,
+        horizontalAccuracy: CLLocationAccuracy,
+        verticalAccuracy: CLLocationAccuracy,
+        course: CLLocationDirection,
+        speed: CLLocationSpeed,
+        timestamp: Date
+        ) {
+
+        super.init(
+            coordinate: coordinate,
+            altitude: altitude,
+            horizontalAccuracy: horizontalAccuracy,
+            verticalAccuracy: verticalAccuracy,
+            course: course,
+            speed: speed,
+            timestamp: timestamp
+        )
+    }
+
+
+    private enum MutableLocationEncodingKeys {
+        static let latitude = "latitude"
+        static let longitude = "longitude"
+        static let altitude = "altitude"
+        static let horizontalAccuracy = "horizontalAccuracy"
+        static let verticalAccuracy = "verticalAccuracy"
+        static let timestamp = "timestamp"
+        static let course = "course"
+        static let speed = "speed"
+        static let floor = "floor"
+    }
+
+    public required init?(coder: NSCoder) {
+        if coder.containsValue(forKey: MutableLocationEncodingKeys.latitude),
+            coder.containsValue(forKey: MutableLocationEncodingKeys.longitude) {
+            _coordinate = CLLocationCoordinate2D(
+                latitude: coder.decodeDouble(forKey: MutableLocationEncodingKeys.latitude),
+                longitude: coder.decodeDouble(forKey: MutableLocationEncodingKeys.longitude)
+            )
+        }
+
+        if coder.containsValue(forKey: MutableLocationEncodingKeys.altitude) {
+            _altitude = coder.decodeDouble(forKey: MutableLocationEncodingKeys.altitude)
+        }
+
+        if coder.containsValue(forKey: MutableLocationEncodingKeys.horizontalAccuracy) {
+            _horizontalAccuracy = coder.decodeDouble(
+                forKey: MutableLocationEncodingKeys.horizontalAccuracy
+            )
+        }
+
+        if coder.containsValue(forKey: MutableLocationEncodingKeys.verticalAccuracy) {
+            _verticalAccuracy = coder.decodeDouble(
+                forKey: MutableLocationEncodingKeys.verticalAccuracy
+            )
+        }
+
+        if coder.containsValue(forKey: MutableLocationEncodingKeys.timestamp),
+            let time = coder.decodeObject(forKey: MutableLocationEncodingKeys.timestamp) as? Date {
+            _timestamp = time
+        }
+
+        if coder.containsValue(forKey: MutableLocationEncodingKeys.course) {
+            _course = coder.decodeDouble(forKey: MutableLocationEncodingKeys.course)
+        }
+
+        if coder.containsValue(forKey: MutableLocationEncodingKeys.speed) {
+            _speed = coder.decodeDouble(forKey: MutableLocationEncodingKeys.speed)
+        }
+
+        if coder.containsValue(forKey: MutableLocationEncodingKeys.floor) {
+            _floor = coder.decodeObject(forKey: MutableLocationEncodingKeys.floor) as? CLFloor
+        }
+
+        super.init(coder: coder)
+    }
+
+    open override func encode(with coder: NSCoder) {
+        super.encode(with: coder)
+
+        if let overriddenCoordinate = _coordinate {
+            coder.encode(
+                overriddenCoordinate.latitude,
+                forKey: MutableLocationEncodingKeys.latitude
+            )
+            coder.encode(
+                overriddenCoordinate.longitude,
+                forKey: MutableLocationEncodingKeys.longitude
+            )
+        }
+
+        if let overriddenAltitude = _altitude {
+            coder.encode(overriddenAltitude, forKey: MutableLocationEncodingKeys.altitude)
+        }
+
+        if let overriddenHorizontalAccuracy = _horizontalAccuracy {
+            coder.encode(
+                overriddenHorizontalAccuracy,
+                forKey: MutableLocationEncodingKeys.horizontalAccuracy
+            )
+        }
+
+        if let overriddenVerticalAccuracy = _verticalAccuracy {
+            coder.encode(
+                overriddenVerticalAccuracy,
+                forKey: MutableLocationEncodingKeys.verticalAccuracy
+            )
+        }
+
+        if let overriddenAltitude = _timestamp {
+            coder.encode(overriddenAltitude, forKey: MutableLocationEncodingKeys.timestamp)
+        }
+
+        if let overriddenCourse = _course {
+            coder.encode(overriddenCourse, forKey: MutableLocationEncodingKeys.course)
+        }
+
+        if let overriddenSpeed = _speed {
+            coder.encode(overriddenSpeed, forKey: MutableLocationEncodingKeys.speed)
+        }
+
+        if let overriddenFloor = _floor {
+            coder.encode(overriddenFloor, forKey: MutableLocationEncodingKeys.floor)
+        }
+    }
+
 }
--- TestableCoreLocationTests/Tests/MutableLocationTests.swift
+++ TestableCoreLocationTests/Tests/MutableLocationTests.swift
@@ -33,7 +33,64 @@ class MutableLocationTests: XCTestCase {
         location = mutableLocation
     }
 
-    func testCanCreateMutableLocation() {
+    func testCanCreateMutableLocationWithCoordinateInitializer() {
+        mutableLocation = MutableLocation(
+            latitude: SampleCoordinate.latitude,
+            longitude: SampleCoordinate.longitude
+        )
+
+        location = mutableLocation
+
+        XCTAssertEqual(location.coordinate, SampleCoordinate,
+                       "Should be able to create a mutable location with coordinate components")
+        XCTAssertEqual(location.altitude, 0,
+                       "Should be able to create a mutable location without an altitude")
+        XCTAssertEqual(location.horizontalAccuracy, 0,
+                       "Should be able to create a mutable location without a horizontal accuracy")
+        XCTAssertEqual(location.verticalAccuracy, -1,
+                       "Should be able to create a mutable location without a vertical accuracy")
+        XCTAssertEqual(location.course, -1,
+                       "Should be able to create a mutable location without a course")
+        XCTAssertEqual(location.speed, -1,
+                       "Should be able to create a mutable location without a speed")
+        XCTAssertEqualWithAccuracy(
+            location.timestamp.timeIntervalSince1970,
+            now.timeIntervalSince1970,
+            accuracy: 1,
+            "Should be able to create a mutable location without a timestamp"
+        )
+        XCTAssertNil(location.floor, "User-created locations do not have floors by default")
+    }
+
+    func testCanCreateMutableLocationWithStationaryInitializer() {
+        mutableLocation = MutableLocation(
+            coordinate: SampleCoordinate,
+            altitude: 5280,
+            horizontalAccuracy: 14.42,
+            verticalAccuracy: 42.14,
+            timestamp: now
+        )
+
+        location = mutableLocation
+
+        XCTAssertEqual(location.coordinate, SampleCoordinate,
+                       "Should be able to create a mutable location with a coordinate")
+        XCTAssertEqual(location.altitude, 5280,
+                       "Should be able to create a mutable location with an altitude")
+        XCTAssertEqual(location.horizontalAccuracy, 14.42,
+                       "Should be able to create a mutable location with a horizontal accuracy")
+        XCTAssertEqual(location.verticalAccuracy, 42.14,
+                       "Should be able to create a mutable location with a vertical accuracy")
+        XCTAssertEqual(location.course, -1,
+                       "Should be able to create a mutable location without a course")
+        XCTAssertEqual(location.speed, -1,
+                       "Should be able to create a mutable location without a speed")
+        XCTAssertEqual(location.timestamp, now,
+                       "Should be able to create a mutable location with a timestamp")
+        XCTAssertNil(location.floor, "User-created locations do not have floors by default")
+    }
+
+    func testCanCreateMutableLocationWithMobileInitializer() {
         XCTAssertEqual(location.coordinate, SampleCoordinate,
                        "Should be able to create a mutable location with a coordinate")
         XCTAssertEqual(location.altitude, 5280,
@@ -48,7 +105,7 @@ class MutableLocationTests: XCTestCase {
                        "Should be able to create a mutable location with a speed")
         XCTAssertEqual(location.timestamp, now,
                        "Should be able to create a mutable location with a timestamp")
-        XCTAssertNil(location.floor, "User-created locations do not have floors")
+        XCTAssertNil(location.floor, "User-created locations do not have floors by default")
     }
 
     func testCoordinateIsMutable() {
@@ -76,8 +133,8 @@ class MutableLocationTests: XCTestCase {
     }
 
     func testCourseIsMutable() {
-        mutableLocation.course = -88
-        XCTAssertEqual(location.course, -88,
+        mutableLocation.course = -88.5
+        XCTAssertEqual(location.course, -88.5,
                        "Should be able to change the course of a location")
     }
 
@@ -97,6 +154,61 @@ class MutableLocationTests: XCTestCase {
         let floor: CLFloor = MutableFloor(level: 14)
         mutableLocation.floor = floor
         XCTAssertEqual(location.floor, floor, "Should be able to change the floor of a location")
+    }
+
+    func testUnmutatedLocationSerialization() {
+        let data = NSKeyedArchiver.archivedData(withRootObject: location)
+        let anotherLocation = NSKeyedUnarchiver.unarchiveObject(with: data) as? CLLocation
+
+        XCTAssertEqual(anotherLocation!.coordinate, SampleCoordinate,
+                       "Should be able to serialize and deserialize a mutable location")
+        XCTAssertEqual(anotherLocation!.altitude, 5280,
+                       "Should be able to serialize and deserialize a mutable location")
+        XCTAssertEqual(anotherLocation!.horizontalAccuracy, 14.42,
+                       "Should be able to serialize and deserialize a mutable location")
+        XCTAssertEqual(anotherLocation!.verticalAccuracy, 42.14,
+                       "Should be able to serialize and deserialize a mutable location")
+        XCTAssertEqual(anotherLocation!.course, 99.44,
+                       "Should be able to serialize and deserialize a mutable location")
+        XCTAssertEqual(anotherLocation!.speed, 10.4,
+                       "Should be able to serialize and deserialize a mutable location")
+        XCTAssertEqual(anotherLocation!.timestamp, now,
+                       "Should be able to serialize and deserialize a mutable location")
+        XCTAssertNil(anotherLocation!.floor,
+                       "Should be able to serialize and deserialize a mutable location")
+    }
+
+    func testMutatedLocationSerialization() {
+        mutableLocation.coordinate = OtherCoordinate
+        mutableLocation.altitude = 13.31
+        mutableLocation.horizontalAccuracy = 18.48
+        mutableLocation.verticalAccuracy = 2.22
+        mutableLocation.course = -88.5
+        mutableLocation.speed = 197.82
+        mutableLocation.timestamp = .distantPast
+
+        let floor: CLFloor = MutableFloor(level: 14)
+        mutableLocation.floor = floor
+
+        let data = NSKeyedArchiver.archivedData(withRootObject: location)
+        let anotherLocation = NSKeyedUnarchiver.unarchiveObject(with: data) as? CLLocation
+
+        XCTAssertEqual(anotherLocation!.coordinate, OtherCoordinate,
+                       "Should be able to serialize and deserialize a mutable location")
+        XCTAssertEqual(anotherLocation!.altitude, 13.31,
+                       "Should be able to serialize and deserialize a mutable location")
+        XCTAssertEqual(anotherLocation!.horizontalAccuracy, 18.48,
+                       "Should be able to serialize and deserialize a mutable location")
+        XCTAssertEqual(anotherLocation!.verticalAccuracy, 2.22,
+                       "Should be able to serialize and deserialize a mutable location")
+        XCTAssertEqual(anotherLocation!.course, -88.5,
+                       "Should be able to serialize and deserialize a mutable location")
+        XCTAssertEqual(anotherLocation!.speed, 197.82,
+                       "Should be able to serialize and deserialize a mutable location")
+        XCTAssertEqual(anotherLocation!.timestamp, .distantPast,
+                       "Should be able to serialize and deserialize a mutable location")
+        XCTAssertEqual(anotherLocation!.floor?.level, floor.level,
+                       "Should be able to serialize and deserialize a mutable location")
     }
 
 }
